51eba1ca9ff46f5981ecbd93fd15fb023795d9ff
Vinh Pham <65750997+viskypham@users.noreply.github.com>
2021-12-04 20:27:57 +0700

    Update Bai2.cpp

--- Bai2.cpp
+++ Bai2.cpp
@@ -5,23 +5,26 @@ int main(){
 	int n;
 	int SoNghichDao=0;
 	printf("So Nguyen Duong n:");
-	scanf("%d",&n);
+	scanf(" %d",&n);
 	if(n<0){ //Khong la so nguyen am
 		printf("Nhap lai so nguyen duong!\n");
 	}else{
-		if(n<9999){ //Khong qua 5 chu so.
-			SoNghichDao = SoNghichDao * 10 + n % 10; //Lay so cuoi 1
-			n = n / 10;								 //Con 3 chu so
-			SoNghichDao = SoNghichDao * 10 + n % 10; //Lay so cuoi 2
-			n = n / 10;								 //Con 2 chu so
-			SoNghichDao = SoNghichDao * 10 + n % 10; //Lay so cuoi 3
-			n = n / 10;							     //Con 1 chu so
-			SoNghichDao = SoNghichDao * 10 + n % 10; //Lay so cuoi 4
-			printf("So Nghich Dao la: %d\n",SoNghichDao);
-		}else{
-			printf("Khong hop le!"); 
+		if(n<9999){ 
+			SoNghichDao = SoNghichDao * 10 + n % 10; 
+			n = n / 10;							
 		}
-		
+		if(n>0){
+			SoNghichDao = SoNghichDao * 10 + n % 10;
+			n = n / 10;
+		}
+		if(n>0){
+			SoNghichDao = SoNghichDao * 10 + n % 10;
+			n = n / 10;
+		}
+		if(n>0){
+			SoNghichDao = SoNghichDao * 10 + n % 10;
+		}
+		printf("So Nghich Dao la: %d",SoNghichDao);
 	}
 		
 
